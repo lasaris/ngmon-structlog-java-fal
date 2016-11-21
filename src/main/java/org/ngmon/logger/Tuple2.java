22a2cb1a12e364f7a582a7777add77aacccf1534
xtovarn <xtovarn@fi.muni.cz>
2016-11-21 11:26:53 +0100

    equals + hashcode

--- src/main/java/org/ngmon/logger/Tuple2.java
+++ src/main/java/org/ngmon/logger/Tuple2.java
@@ -17,4 +17,22 @@ public class Tuple2<T0, T1> {
                 ", f1=" + f1 +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tuple2)) return false;
+
+        Tuple2<?, ?> tuple2 = (Tuple2<?, ?>) o;
+
+        return f0.equals(tuple2.f0) && f1.equals(tuple2.f1);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = f0.hashCode();
+        result = 31 * result + f1.hashCode();
+        return result;
+    }
 }
