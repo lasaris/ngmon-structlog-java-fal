69ca26877680727e4e8a6fcaca06f0e50c5bb971
xtovarn <xtovarn@fi.muni.cz>
2016-11-21 13:26:55 +0100

    we are logging

--- src/main/java/com/example/SimpleLogger.java
+++ src/main/java/com/example/SimpleLogger.java
@@ -1,12 +1,38 @@
 package com.example;
 
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.reflect.ReflectDatumWriter;
 import org.ngmon.logger.EventLevel;
 import org.ngmon.logger.LogEvent;
 import org.ngmon.logger.Logger;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 public class SimpleLogger implements Logger {
 
-	public void log(EventLevel level, LogEvent dataMap) {
-		System.err.println(level + " :: " + dataMap);
-	}
+    public void log(EventLevel level, LogEvent logEvent) {
+        Schema schema = logEvent.getSchema();
+        GenericRecord data = logEvent.getData(schema);
+
+        try {
+            System.out.println(new String(serialize(schema, data)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    byte[] serialize(Schema schema, GenericRecord record) throws IOException {
+        ReflectDatumWriter<GenericRecord> datumWriter = new ReflectDatumWriter<>(schema);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Encoder encoder = EncoderFactory.get().jsonEncoder(schema, out, false);
+        datumWriter.write(record, encoder);
+        encoder.flush();
+        byte[] bytes = out.toByteArray();
+        out.close();
+        return bytes;
+    }
 }
--- src/main/java/org/ngmon/logger/LogEvent.java
+++ src/main/java/org/ngmon/logger/LogEvent.java
@@ -1,5 +1,11 @@
 package org.ngmon.logger;
 
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.reflect.ReflectData;
+
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +14,7 @@ public class LogEvent {
 
     private Map<String, Integer> existenceMap = new HashMap<>();
     private Map<Tuple2<String, Type>, Object> valueMap = new HashMap<>();
+    private ReflectData reflectData = new ReflectData();
 
     void setMessage(String message) {
         this.put("message", String.class, message);
@@ -42,6 +49,25 @@ public class LogEvent {
         } else {
             return paramName;
         }
+    }
+
+    public Schema getSchema() {
+        SchemaBuilder.FieldAssembler<Schema> fieldAssembler = SchemaBuilder.record(getSignature()).namespace("logger").fields();
+        for (Tuple2<String, Type> tuple2 : this.valueMap.keySet()) {
+            fieldAssembler = fieldAssembler.name(tuple2.f0).type(this.reflectData.getSchema(tuple2.f1)).noDefault();
+        }
+        return fieldAssembler.endRecord();
+    }
+
+    public GenericRecord getData(Schema schema) {
+        GenericRecord data = new GenericData.Record(schema);
+        for (Map.Entry<Tuple2<String, Type>, Object> tuple2ObjectEntry : this.valueMap.entrySet()) {
+            Tuple2<String, Type> key = tuple2ObjectEntry.getKey();
+            String name = key.f0;
+            Object value = tuple2ObjectEntry.getValue();
+            data.put(name, value);
+        }
+        return data;
     }
 
     @Override
