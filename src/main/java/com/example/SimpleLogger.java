2e7f7647a8b8dfc045e4c3dbd73f7bb5b8dbecc3
xtovarn <xtovarn@fi.muni.cz>
2016-11-22 14:30:05 +0100

    create dir

--- src/main/java/com/example/SimpleLogger.java
+++ src/main/java/com/example/SimpleLogger.java
@@ -5,7 +5,9 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.Encoder;
 import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.reflect.ReflectDatumWriter;
+import org.apache.avro.specific.SpecificData;
 import org.ngmon.logger.EventLevel;
+import org.ngmon.logger.EventWrapper;
 import org.ngmon.logger.LogEvent;
 import org.ngmon.logger.Logger;
 
@@ -13,6 +15,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,14 +30,7 @@ public class SimpleLogger implements Logger {
         if (!this.schemaMap.containsKey(signature)) {
             schema = logEvent.getSchema();
             this.schemaMap.put(signature, schema);
-
-            try {
-                FileOutputStream out = new FileOutputStream("schemas/" + signature + ".avsc");
-                out.write(schema.toString(true).getBytes());
-                out.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            createShemaFile(signature, schema);
         } else {
             schema = this.schemaMap.get(signature);
         }
@@ -47,7 +44,19 @@ public class SimpleLogger implements Logger {
         }
     }
 
-    byte[] serialize(Schema schema, GenericRecord record) throws IOException {
+    private void createShemaFile(String signature, Schema schema) {
+        try {
+            String dir = "schemas/";
+            Files.createDirectories(Paths.get(dir));
+            FileOutputStream out = new FileOutputStream(dir + signature + ".avsc");
+            out.write(schema.toString(true).getBytes());
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private byte[] serialize(Schema schema, GenericRecord record) throws IOException {
         ReflectDatumWriter<GenericRecord> datumWriter = new ReflectDatumWriter<>(schema);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         Encoder encoder = EncoderFactory.get().jsonEncoder(schema, out, false);
