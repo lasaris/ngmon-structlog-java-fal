7dee1a8f279a63520042f18f021b1d30c7f3a88c
xtovarn <xtovarn@fi.muni.cz>
2016-11-22 15:48:07 +0100

    serializer creates schema

--- src/main/java/com/example/SimpleLogger.java
+++ src/main/java/com/example/SimpleLogger.java
@@ -2,63 +2,44 @@ package com.example;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.Encoder;
-import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.reflect.ReflectData;
-import org.apache.avro.reflect.ReflectDatumWriter;
-import org.apache.avro.specific.SpecificData;
 import org.ngmon.logger.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SimpleLogger implements Logger {
 
-    private Map<String, Schema> schemaMap = new HashMap<>();
+    private Map<String, Tuple2<Schema, Serializer<GenericRecord>>> schemaMap = new HashMap<>();
     private final Schema wrapperSchema = new ReflectData().getSchema(EventWrapper.class);
     private final Serializer<EventWrapper> mainSerializer = new Serializer<>(wrapperSchema);
 
     public void log(EventLevel level, LogEvent logEvent, String signature) {
-        Schema schema = getSchema(signature, logEvent);
-        Serializer<GenericRecord> payloadSerializer = new Serializer<>(schema);
+        Tuple2<Schema, Serializer<GenericRecord>> tuple2 = getCachedType(signature, logEvent);
         try {
             long timestamp = System.currentTimeMillis();
-            byte[] payload = payloadSerializer.serialize(logEvent.getData(schema));
-            byte[] eventWrapperBytes = mainSerializer.serialize(new EventWrapper(signature, timestamp, level, payload));
+            byte[] payload = tuple2.f1.serialize(logEvent.getData(tuple2.f0));
+            byte[] eventWrapperBytes = this.mainSerializer.serialize(new EventWrapper(signature, timestamp, level, payload));
             System.out.println(new String(eventWrapperBytes));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private Schema getSchema(String signature, LogEvent logEvent) {
-        Schema schema;
+    private Tuple2<Schema, Serializer<GenericRecord>> getCachedType(String signature, LogEvent logEvent) {
+        Tuple2<Schema, Serializer<GenericRecord>> tuple2;
         if (!this.schemaMap.containsKey(signature)) {
-            schema = logEvent.getSchema();
-            this.schemaMap.put(signature, schema);
-            createShemaFile(signature, schema);
+            Schema schema = logEvent.getSchema();
+            Serializer<GenericRecord> serializer = new Serializer<>(schema);
+            tuple2 = new Tuple2<>(schema, serializer);
+            this.schemaMap.put(signature, tuple2);
         } else {
-            schema = this.schemaMap.get(signature);
+            tuple2 = this.schemaMap.get(signature);
         }
-        return schema;
-    }
-
-    private void createShemaFile(String signature, Schema schema) {
-        try {
-            String dir = "schemas/";
-            Files.createDirectories(Paths.get(dir));
-            FileOutputStream out = new FileOutputStream(dir + signature + ".avsc");
-            out.write(schema.toString(true).getBytes());
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return tuple2;
     }
 }
--- src/main/java/org/ngmon/logger/Serializer.java
+++ src/main/java/org/ngmon/logger/Serializer.java
@@ -1,13 +1,15 @@
 package org.ngmon.logger;
 
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.Encoder;
 import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.reflect.ReflectDatumWriter;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Serializer<T> {
 
@@ -18,6 +20,7 @@ public class Serializer<T> {
     public Serializer(Schema schema) {
         this.schema = schema;
         this.datumWriter = new ReflectDatumWriter<>(schema);
+        createSchemaFile();
     }
 
     public byte[] serialize(T record) throws IOException {
@@ -28,5 +31,17 @@ public class Serializer<T> {
         byte[] bytes = out.toByteArray();
         out.close();
         return bytes;
+    }
+
+    private void createSchemaFile() {
+        try {
+            String dir = "schemas/" + this.schema.getNamespace().replace(".", "/") + "/";
+            Files.createDirectories(Paths.get(dir));
+            FileOutputStream out = new FileOutputStream(dir + this.schema.getName() + ".avsc");
+            out.write(this.schema.toString(true).getBytes());
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
