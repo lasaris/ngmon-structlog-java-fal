e8f781bafc8d194b9e5da70625ebc878b941261e
xtovarn <xtovarn@fi.muni.cz>
2017-01-05 14:46:51 +0100

    * logging proper events

--- src/main/java/com/example/SimpleLogger.java
+++ src/main/java/com/example/SimpleLogger.java
@@ -1,30 +1,45 @@
 package com.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ngmon.logger.Logger;
 import org.ngmon.logger.enums.EventLevel;
 import org.ngmon.logger.injection.LogEvent;
-import org.ngmon.logger.serialize.CachingSchemaGenerator;
-import org.ngmon.logger.serialize.EventWrapper;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
 
 public class SimpleLogger implements Logger {
 
-    private final ObjectMapper mapper = new ObjectMapper();
+    private JsonFactory jsonFactory = new JsonFactory();
 
     public SimpleLogger() throws JsonMappingException {
     }
 
     @Override
     public void log(EventLevel level, LogEvent logEvent, String signature) {
-        try {
-            long timestamp = System.currentTimeMillis();
-            String payload = this.mapper.writeValueAsString(logEvent.getValueMap());
-            String record = this.mapper.writeValueAsString(new EventWrapper(signature, timestamp, level, payload));
-            System.out.println(record);
-        } catch (JsonProcessingException e) {
+        System.out.println(getEventJson("events", signature, logEvent.getValueMap(), level));
+    }
+
+    private String getEventJson(String fqnNS, String event, Map<String, Object> payload, EventLevel level) {
+        StringWriter writer = new StringWriter();
+        try (JsonGenerator json = this.jsonFactory.createGenerator(writer)) {
+            json.writeStartObject();
+            json.writeStringField("sid", event);
+            json.writeStringField("level", level.toString());
+            json.writeStringField("sid_ns", fqnNS);
+            json.writeObjectFieldStart("_");
+            for (Map.Entry<String, Object> stringObjectEntry : payload.entrySet()) {
+                json.writeObjectField(stringObjectEntry.getKey(), stringObjectEntry.getValue());
+            }
+            json.writeEndObject();
+            json.writeEndObject();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return writer.toString();
     }
 }
