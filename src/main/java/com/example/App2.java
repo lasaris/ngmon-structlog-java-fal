ac2ca74af9b7487fb86a9ce4d48e52288c661d5a
xtovarn <xtovarn@fi.muni.cz>
2017-08-29 13:24:29 +0200

    cleanup for thesis

--- src/main/java/com/example/App2.java
+++ src/main/java/com/example/App2.java
@@ -14,16 +14,16 @@ public class App2 {
         String reason = "reason";
 
         LOGX.info("...oval for dataNode from PENDING_UNCACHED - it was uncached by the dataNode.")
-                .blockId(blockId).dataNodeUuid(datanodeUuid).log();//Event_N1656664914_P657257274
+                .blockId(blockId).dataNodeUuid(datanodeUuid).log();     //Event_2d9cfc60_d9e20513
         LOGX.info("Cannot cache block")
-                .blockId(blockId).reason(reason).log();            //Event_P1920713228_P1300017906
+                .blockId(blockId).reason(reason).log();                 //Event_205317be_7dc94af1
         LOGX.info("... for dataNode from PENDING_CACHED - we already have enough cached replicas")
                 .blockId(blockId).dataNodeUuid(datanodeUuid).numCached(numCached)
-                .neededCached(neededCached).log();                 //Event_P545087615_N1858460731
+                .neededCached(neededCached).log();                      //Event_dd3ebb4e_f046faee
         LOGX.info("...for dataNode from PENDING_UNCACHED - we do not have enough cached replicas")
                 .blockId(blockId).dataNodeUuid(datanodeUuid).numCached(numCached)
-                .neededCached(neededCached).log();                 //Event_P545087615_P609856424
+                .neededCached(neededCached).log();                      //Event_dd3ebb4e_a7e9c059
         LOGX.info("...ocks - neededCached == 0, and pendingUncached and pendingCached are empty.")
-                .blockId(blockId).blockId(blockId + 10).log();     //Event_P304007109_N1938941448
+                .blockId(blockId).blockId(blockId + 10).log();          //Event_616b8b66_53bc3e0f
     }
 }
